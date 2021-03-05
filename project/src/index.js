44ebd5ca59a1c2ef228282fb1ec9025cc3dffd52
Arsenii <mingazov-201@bk.ru>
2021-03-05 13:03:38 +0300

    Checking hook v3.0

--- project/src/index.js
+++ project/src/index.js
@@ -1,12 +1,13 @@
 const Koa = require('koa');
 
 const { PORT } = process.env;
+
 const app = new Koa();
 
 // response
 app.use(async (ctx) => {
   const obj = JSON.stringify(ctx.query);
   ctx.body = `Hello World, your parameters are ${obj}`;
-  console.log('Commit with 3 errors');
 });
+
 app.listen(PORT);
