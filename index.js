ea7b2a72ff0aceff82498a16a029f6725934466e
Arsenii <mingazov-201@bk.ru>
2021-03-10 16:33:45 +0300

    Added koa-json

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Koa = require('koa');
+const json = require('koa-json');
 const apiRouter = require('./app/routes');
 
 const app = new Koa();
 const { PORT } = process.env;
 
+app.use(json());
 app.use(apiRouter.routes()).use(apiRouter.allowedMethods());
 
 app.listen(PORT);
