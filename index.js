46a4d0db052326227dd803ea88f9a6a3c0e53e29
Arsenii <mingazov-201@bk.ru>
2021-03-10 16:53:27 +0300

    Added koa-logger

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const Koa = require('koa');
 const json = require('koa-json');
 const bodyParser = require('koa-bodyparser');
+const logger = require('koa-logger');
 const apiRouter = require('./app/routes');
 
 const app = new Koa();
 const { PORT } = process.env;
 
+app.use(logger());
 app.use(apiRouter.routes()).use(apiRouter.allowedMethods());
 app.use(json());
 app.use(bodyParser());
