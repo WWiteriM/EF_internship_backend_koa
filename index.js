153d2f30e11f14201a4961ab7a127ce7c6ce29d5
Arsenii <mingazov-201@bk.ru>
2021-03-10 16:46:02 +0300

    Added koa-bodyparser

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const Koa = require('koa');
 const json = require('koa-json');
+const bodyParser = require('koa-bodyparser');
 const apiRouter = require('./app/routes');
 
 const app = new Koa();
 const { PORT } = process.env;
 
-app.use(json());
 app.use(apiRouter.routes()).use(apiRouter.allowedMethods());
+app.use(json());
+app.use(bodyParser());
 
 app.listen(PORT);
